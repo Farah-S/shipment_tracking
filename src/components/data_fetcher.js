f7ead95bcaed2fc1ba90b42e224d5e0c2d3362e6
Farah Sherif <farah.faroha132@gmail.com>
2024-02-26 22:01:28 +0200

    data fetcher

---
+++ src/components/data_fetcher.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+const DataFetcher = (id) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://tracking.bosta.co/shipments/track/'+id); // Replace with your API endpoint
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      const data = await response.json();
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+ return data;
+};
+
+export default DataFetcher;
