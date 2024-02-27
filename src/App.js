ecbcbf928480e29f4d12e3a95ec0979b7cd06d5e
Farah Sherif <farah.faroha132@gmail.com>
2024-02-27 16:50:06 +0200

    font colors + title

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import NavHeader from './components/header';
 import ShipmentDetails from './components/shipment_details';
 
 function App() {
-  
+  //status values: DELIVERED_TO_SENDER, DELIVERED, CANCELLED
   return (
     <div className="App">
       <header className='App-header'>
         <NavHeader/>
       </header>
       <body className='App-body'>
-        <ShipmentDetails id='15248' status="done" lastUpdate="25/2/2022" deliveryDate="2 jan 2002" retailer="Souq.com"/>
+        <ShipmentDetails id='15248' status="CANCELLED" lastUpdate="25/2/2022" deliveryDate="2 jan 2002" retailer="Souq.com"/>
       </body>
     </div>
   );
--- src/components/shipment_details.js
+++ src/components/shipment_details.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import {Col, Row} from 'react-bootstrap';
 function ShipmentDetails ({id,status, lastUpdate, retailer, deliveryDate}) {
- return (
+    //status values: DELIVERED_TO_SENDER, DELIVERED, CANCELLED
+    var font='#F9BA02';
+    switch (status) {
+        case "DELIVERED":
+            status="Delivered";
+            font="green";
+            break;
+        case "CANCELLED":
+             font="red";
+            status="Cancelled";
+            break;
+        default:
+            status="Not Delivered";
+            break;
+    }
+    
+    var style={color:font};
+    
+    return (
         <Row className='Details'>
             <Col>
                 <Row className='Grey-font' >Shipment Number {id}</Row>
-                <Row className='Black-font'>{status}</Row>
+                <Row className='Black-font' style={style}>{status}</Row>
             </Col>
             <Col>
                 <Row className='Grey-font' >Last Update</Row>
@@ -20,8 +38,8 @@ function ShipmentDetails ({id,status, lastUpdate, retailer, deliveryDate}) {
                 <Row className='Black-font'>{deliveryDate}</Row>
             </Col>
         </Row>
-    
-     
+        
+        
     );
 };
 
