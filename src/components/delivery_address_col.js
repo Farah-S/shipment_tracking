1c622c3002bd9c069f88af171e7a0618d059f2a6
Farah Sherif <farah.faroha132@gmail.com>
2024-02-28 19:29:11 +0200

    problem box

--- src/components/delivery_address_col.js
+++ src/components/delivery_address_col.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import './delivery_address_col.css';
-import {Col, Row} from 'react-bootstrap';
+import {Button, Col, Row} from 'react-bootstrap';
+import question_mark from '../assets/images/question_mark.jpg';
 
 function DeliveryAddressCol({address}) {
     //status values: DELIVERED_TO_SENDER, DELIVERED, CANCELLED
     
     return (
-        <Row className='Address-box'>
-            <p style={{flexWrap:"wrap", width: "22vw"}}>
-                {address}
-                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-            </p> 
-        </Row>  
+        <div>
+            <Row className='Address-box'>
+                <p style={{flexWrap:"wrap", width: "22vw"}}>
+                    {address}
+                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                </p>
+            </Row>
+            <Row className='Problem-box'>
+                <Col className='Problem-col'>
+                    <Row style={{flexWrap:"wrap", paddingBottom:"1vh", alignContent:"center"}}>
+                    Problem with your shipment?!
+                    </Row>
+                    <Button className='Problem-button'>Report Problem</Button>
+                </Col>
+                <Col className='Image-col'>
+                    <img src={question_mark} style={{width:"7vw"}}/>
+                </Col>
+            </Row>
+        </div>
     );
 };
 
